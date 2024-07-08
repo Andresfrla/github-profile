60199367147a9c874e3bb1eb1aa3f908213d9406
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-07-07 18:27:08 -0600

    modify the search bar to adjust

--- src/app/components/_SearchBar/page.jsx
+++ src/app/components/_SearchBar/page.jsx
@@ -4,8 +4,8 @@ import SearchIcon from '/public/Search.svg';
 
 const SearchBar = () => {
   return (
-    <div className='relative mt-8 rounded-md bg-[#20293A] h-12 w-2/3'>
-      <div className='absolute left-0 top-0 h-full flex items-start pl-2 mt-2 '>
+    <div className='relative mt-8 rounded-xl bg-[#20293A] h-14 w-3/4 items-center'>
+      <div className='absolute left-0 top-0 h-full flex items-start pl-4 mt-2 pt-2'>
         <Image 
           width={24}
           height={24}
@@ -16,7 +16,7 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="username"
-        className='pl-10 bg-transparent w-full h-10 focus:outline-none'
+        className='pl-14 pt-3 bg-transparent w-full h-10 focus:outline-none'
       />
     </div>
   );
