85c310fd1769cacb0baefd0444e1c954f1a1d330
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-07-08 21:44:43 -0600

    add the default profile of github before you search

--- src/app/_components/_Profile/page.jsx
+++ src/app/_components/_Profile/page.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import ProfileData from './_components/_ProfileData/ProfileData';
 
 const Profile = ({ userData }) => {
-  console.log(userData);
+  const [githubInfo, setGithubInfo] = useState({});
+
+  useEffect(() => {
+    const fetchGithubInfo = async () => {
+      try {
+        const username = userData?.username || 'github'; // Reemplaza 'github' con un usuario por defecto o maneja el caso apropiadamente
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        setGithubInfo(data);
+      } catch (error) {
+        console.log('Error fetching GitHub data:', error);
+      }
+    };
+
+    fetchGithubInfo();
+  }, [userData]);
 
   return (
     <div className='mt-40'>
-      {userData && (
-        <div>
-          <img 
-            src={userData.avatar_url} 
-            alt="User Avatar" 
-            className='size-28'
+      <div>
+        {userData?.avatar_url ? (
+          <img
+            src={userData.avatar_url}
+            alt="User Avatar"
+            className='size-28 rounded-2xl'
+          />
+        ) : (
+          githubInfo.avatar_url && (
+            <img
+              src={githubInfo.avatar_url}
+              alt="GitHub User Avatar"
+              className='size-28 rounded-2xl'
             />
-        </div>
-      )}
+          )
+        )}
+        <ProfileData userData={userData || githubInfo} /> {/* Pass the appropriate data to ProfileData */}
+      </div>
     </div>
   );
 };
