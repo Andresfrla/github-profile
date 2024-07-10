9e2bfd02ba589c16c73ffe0c9ad843aa984ec243
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-07-09 22:04:22 -0600

    Profile Data done

--- src/app/_components/_Profile/_components/_ProfileData/ProfileData.jsx
+++ src/app/_components/_Profile/_components/_ProfileData/ProfileData.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const ProfileData = ({userData}) => {
   return (
     <div className='flex flex-col'>
-        <div className='flex bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
+        <div className='flex w-fit bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
             <h3 className='pr-3'>Followers</h3>
             <span className='text-4xl mt-[-8px] mb-[-10px] font-thin'>|</span>
             <p className='text-[#CDD5E0] pl-3'>{userData.followers}</p>
         </div>
-        <div className='flex bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
+        <div className='flex w-fit bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
             <h3 className='pr-3'>Following</h3>
             <span className='text-4xl mt-[-8px] mb-[-10px] font-thin'>|</span>
             <p className='text-[#CDD5E0] pl-3'>{userData.following}</p>
         </div>
-        <div className='flex bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
+        <div className='flex w-fit bg-[#111729] py-4 px-5 rounded-xl ml-10 mt-4 text-[#4A5567]'>
             <h3 className='pr-3 '>Location</h3>
             <span className='text-4xl mt-[-8px] mb-[-10px] font-thin'>|</span>
             <p className='text-[#CDD5E0] pl-3'>{userData.location}</p>
