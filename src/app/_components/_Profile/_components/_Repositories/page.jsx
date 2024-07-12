1e28bc502f2dc23e787947bbb9ccfa6e00932619
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-07-11 19:01:12 -0600

    use the map to show only the first repos

--- src/app/_components/_Profile/_components/_Repositories/page.jsx
+++ src/app/_components/_Profile/_components/_Repositories/page.jsx
@@ -9,13 +9,19 @@ const Repositories = ({ userData }) => {
      .then(data => setRepos(data));
   }, [userData.repos_url]);
 
+  console.log(userData.repos_url)
+
+
   return (
     <div>
       <h2 className='text-large mt-8 ml-16'>{userData.name}</h2>
       <p className='text-[#CDD5E0] font-extralight text-title ml-16 mt-3'>{userData.bio}</p>
       <ul>
-        {repos.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+        {repos.slice(0, 5).map(repo => (
+          <div>
+            <h4 key={repo.id} className='text-title'>{repo.name}</h4>
+            <p key={repo.id} className='text-[#CDD5E0] font-extralight text-body'>{repo.description}</p>
+          </div>
         ))}
       </ul>
     </div>
